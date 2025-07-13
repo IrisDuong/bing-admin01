ab97fa9e703eb561a09abfeb38757ec23f7453eb
Manhnk2 <manhtt09@gmail.com>
2025-07-13 16:23:38 +0700

    tao bing-admin gom fe va be

---
+++ bing-admin-be/src/main/java/com/bing/BingAdminBeApplication.java
@@ -0,0 +1,13 @@
+package com.bing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingAdminBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BingAdminBeApplication.class, args);
+	}
+
+}
